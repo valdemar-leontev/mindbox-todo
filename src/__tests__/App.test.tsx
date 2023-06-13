68858268ee2466db557893efbd4141683202ddda
valdemar-leontev <valdemar.leontev@gmail.com>
2023-06-13 13:19:47 +0300

    more tests

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,12 +1,13 @@
 import '@testing-library/jest-dom/extend-expect';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 
 import React from 'react';
 import { Provider } from 'react-redux';
 import { App } from '../App';
 import store from '../store/store';
+import { Footer } from '../components/footer/footer';
 
-describe('App component', () => {
+describe('App', () => {
   it('should render todos text when rendered on screen with redux and provider setup', () => {
     const { getByText } = render(
       <Provider store={store}>
@@ -14,5 +15,27 @@ describe('App component', () => {
       </Provider>,
     );
     expect(getByText("todos")).toBeInTheDocument();
+  });
+});
+
+describe('Footer', () => {
+  it('check filters', () => {
+    const { getByText } = render(
+      <Provider store={store}>
+        <Footer />
+      </Provider>,
+    );
+    expect(getByText("All")).toBeInTheDocument();
+    expect(getByText("Active")).toBeInTheDocument();
+    expect(getByText("Completed")).toBeInTheDocument();
+  });
+
+  it('check clear completed tasks title', () => {
+    const { getByText } = render(
+      <Provider store={store}>
+        <Footer />
+      </Provider>,
+    );
+    expect(getByText("Clear completed")).toBeInTheDocument();
   });
 });
