58d51d2d6d95e6524f28026e019671d62c6bcbe5
valdemar-leontev <valdemar.leontev@gmail.com>
2023-06-13 12:59:46 +0300

    redo test

---
+++ src/__tests__/App.test.tsx
@@ -0,0 +1,18 @@
+import '@testing-library/jest-dom/extend-expect';
+import { render, screen } from '@testing-library/react';
+
+import React from 'react';
+import { Provider } from 'react-redux';
+import { App } from '../App';
+import store from '../store/store';
+
+describe('App component', () => {
+  it('should render todos text when rendered on screen with redux and provider setup', () => {
+    const { getByText } = render(
+      <Provider store={store}>
+        <App />
+      </Provider>,
+    );
+    expect(getByText("todos")).toBeInTheDocument();
+  });
+});
